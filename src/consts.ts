adfba582913a0f9c9c6d1b54582612baf5cdf4c3
Velka <contact@velka.dev>
2024-04-13 23:42:59 +0200

    chore: content modifications

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "Velka Dev",
+  DESCRIPTION: "Welcome to my personal website, a portfolio and blog to showcase my work and thoughts.",
+  AUTHOR: "Velka",
 }
 
 // Work Page
@@ -56,26 +56,20 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "contact@velka.dev",
+    HREF: "mailto:contact@velka.dev",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "Velka-DEV",
+    HREF: "https://github.com/Velka-DEV"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
-  },
-  { 
-    NAME: "Twitter",
-    ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+    TEXT: "cedric-try",
+    HREF: "https://www.linkedin.com/in/cedric-try/",
   },
 ]
 
